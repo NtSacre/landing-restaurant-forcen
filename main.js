cfe706d05d32a8f3270fa9aed7f9ccc480c7b39d
Honore890 <aklessopoukpedi7@gmail.com>
2024-12-23 03:35:59 +0000

    ajout du menu popup

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ const nav = document.getElementById("navi-links");
 nav.classList.toggle("active");
 
 });
+
+const openButton=document.getElementById('openButton');
+const closeButton=document.getElementById('closeButton');
+const popup=document.getElementById('popup');
+const popupOverlay=document.getElementById('popupOverlay');
+
+function openMenuPopup(){
+    popup.style.display='block';
+    popupOverlay.style.display='block';
+}
+
+function closeMenuPopup(){
+    popup.style.display='none';
+    popupOverlay.style.display='none';
+}
+
+openButton.addEventListener('click',openMenuPopup);
+closeButton.addEventListener('click',closeMenuPopup);
